82d911cb0bcf0d17c93fde3af251dcf2de305f4f
Rollin Thomas <rcthomas@lbl.gov>
2018-01-04 10:18:41 -0800

    Removing the tmp homedir hack to see if still work
    (Should probably)

--- batchspawner/batchspawner.py
+++ batchspawner/batchspawner.py
@@ -95,8 +95,8 @@ class BatchSpawnerBase(Spawner):
     req_homedir = Unicode()
     @default('req_homedir')
     def _req_homedir_default(self):
-        return "/tmp"                                   # HACK
-#       return pwd.getpwnam(self.user.name).pw_dir      # HACK
+#       return "/tmp"                                   # HACK
+        return pwd.getpwnam(self.user.name).pw_dir      # HACK
 
     req_keepvars = Unicode()
     @default('req_keepvars')
