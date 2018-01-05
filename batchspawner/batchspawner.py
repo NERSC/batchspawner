d3276b86f328c8ecda7e086c81488f949e8adf4d
Rollin Thomas <rcthomas@lbl.gov>
2018-01-05 15:34:45 -0800

    Full path to slurm commands

--- batchspawner/batchspawner.py
+++ batchspawner/batchspawner.py
@@ -541,11 +541,11 @@ unset XDG_RUNTIME_DIR
     prefix = "ssh -q -o StrictHostKeyChecking=no -o preferredauthentications=publickey -l {username} -i /tmp/{username}.key {remote_host} "
 
     # outputs line like "Submitted batch job 209"
-    batch_submit_cmd = Unicode(prefix + 'sbatch').tag(config=True)
+    batch_submit_cmd = Unicode(prefix + '/usr/bin/sbatch').tag(config=True)
     # outputs status and exec node like "RUNNING hostname"
 #   batch_query_cmd = Unicode(prefix + 'squeue -h -j {job_id} -o \\"%T %B\\"').tag(config=True) # Added backslashes here for quoting
     batch_query_cmd = Unicode(prefix + '/usr/bin/python /global/common/shared/das/sdn/getip.py {job_id}').tag(config=True) # Added backslashes here for quoting
-    batch_cancel_cmd = Unicode(prefix + 'scancel {job_id}').tag(config=True)
+    batch_cancel_cmd = Unicode(prefix + '/usr/bin/scancel {job_id}').tag(config=True)
     # use long-form states: PENDING,  CONFIGURING = pending
     #  RUNNING,  COMPLETING = running
     state_pending_re = Unicode(r'^(?:PENDING|CONFIGURING)').tag(config=True)
